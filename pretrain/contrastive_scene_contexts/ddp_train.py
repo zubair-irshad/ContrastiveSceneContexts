d15f727b7c7b98ef3d7ed5a2d7207b65b6ffbc4d
Zubair Irshad <Muhammad Zubair Irshad>
2024-02-25 23:13:01 -0800

    local changes to run it without minkowski

--- pretrain/contrastive_scene_contexts/ddp_train.py
+++ pretrain/contrastive_scene_contexts/ddp_train.py
@@ -104,6 +104,13 @@ def main(config):
 #         dist.destroy_process_group()
 
 def single_proc_run(config):
+
+  os.environ['MASTER_ADDR'] = 'localhost'
+  os.environ['MASTER_PORT'] = '12355'
+
+  # initialize the process group
+  dist.init_process_group(backend='nccl', rank=torch.cuda.device_count(), world_size=1)
+  
   from lib.ddp_data_loaders import make_data_loader
 
   train_loader = make_data_loader(
